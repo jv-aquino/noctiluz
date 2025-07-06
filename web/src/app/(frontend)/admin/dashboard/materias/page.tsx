5a93ea500de59753d99e1dbbbf9ec57a9ed8f793
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-07-06 11:16:40 -0300

    feat: add swr & materia fetching (w topicos)

--- web/src/app/(frontend)/admin/dashboard/materias/page.tsx
+++ web/src/app/(frontend)/admin/dashboard/materias/page.tsx
@@ -1,13 +1,26 @@
+'use client'
+import type { MateriaWithTopico } from "@/types";
 import { LibraryBig, Plus } from "lucide-react";
+
+import useSWR from 'swr'
 import AdminHeader from "../components/header/AdminHeader";
 
+const fetcher = (url: string) =>
+  fetch(url).then((res) => {
+    if (!res.ok) throw new Error("Erro ao buscar matérias")
+    return res.json()
+  })
+
 function DashboardPage() {
+  const { data: materias, error, isLoading } = useSWR<MateriaWithTopico[]>('/api/materia', fetcher)
+
   const Paragraph = () => (
     <>
       São as bases/grandes áreas de conhecimento. A edição aqui é mais estética<br/>
       e serve para depois adicionar tópicos cuja origem é essa matéria
     </>
   )
+
   return ( 
     <>
       <AdminHeader
@@ -19,6 +32,12 @@ function DashboardPage() {
           <Plus /> Adicionar Matérias
         </button>
       </AdminHeader>
+
+      <main className="grid">
+        {isLoading && <p>Carregando matérias...</p>}
+        {error && <p>Erro ao carregar matérias.</p>}
+        {materias?.forEach(materia => console.log(materia))}
+      </main>
     </>
    );
 }
